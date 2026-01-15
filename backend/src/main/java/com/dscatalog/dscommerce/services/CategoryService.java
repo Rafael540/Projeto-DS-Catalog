d3b7fc4b9a411517d670769adfd37726af9741d3
Rafael540 <alves540@hotmail.com>
2026-01-15 16:56:41 -0300

    db seed

--- backend/src/main/java/com/dscatalog/dscommerce/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/dscommerce/services/CategoryService.java
@@ -4,8 +4,10 @@ import com.dscatalog.dscommerce.entities.Category;
 import com.dscatalog.dscommerce.repositories.CategoryRepository;
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -13,6 +15,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
